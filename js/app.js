e291ae1062f1839de3f71dd089a0c1776050a0b2
Jorge Monzon <jorge29luis10@gmail.com>
2023-03-08 13:15:06 -0500

    Validando formulario

--- js/app.js
+++ js/app.js
@@ -11,5 +11,28 @@ document.addEventListener('DOMContentLoaded', () => {
 function agregarTweet(evento) {
   evento.preventDefault();
 
-  console.log('agregando');
+  // TextArea donde el usuario escribe
+  const tweet = formulario.querySelector('#tweet').value;
+
+  if (tweet === '') {
+    mostrarError('Un mensaje no puede ir vacio');
+    return; //previene ejecución de más lineas de código
+  }
+  console.log('Agregando tweet');
+}
+
+// Mostrar un mensaje de error
+function mostrarError(mensaje) {
+  const mensajeError = document.createElement('P');
+  mensajeError.textContent = mensaje;
+  mensajeError.classList.add('error');
+
+  // Insertarlo en el Contenido
+  const contenido = document.querySelector('#contenido');
+  contenido.appendChild(mensajeError);
+
+  // Elimina la alerta despues de 3 segundos
+  setTimeout(() => {
+    mensajeError.remove();
+  }, 3000);
 }
